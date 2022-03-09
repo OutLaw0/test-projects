8284488bfe66c0c515e943a2b9da6600616ad866
OutLawO <vaakimov@gmail.com>
2022-03-09 23:40:54 +0300

    feat: add chang lang

--- virtual-keyboard/dist/scripts.js
+++ virtual-keyboard/dist/scripts.js
@@ -1,12 +1,12 @@
 /*
- * ATTENTION: An "eval-source-map" devtool has been used.
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (function() { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
@@ -14,9 +14,9 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style/main.scss */ \"./src/style/main.scss\");\n\n/*start keyboard*/\n\nvar keyboard = document.querySelector(\".keyboard\");\nvar keyboard__keys = document.querySelector(\".keyboard__keys\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJtYXBwaW5ncyI6Ijs7QUFBQTtBQUVBOztBQUVBLElBQU1BLFFBQVEsR0FBR0MsUUFBUSxDQUFDQyxhQUFULENBQXVCLFdBQXZCLENBQWpCO0FBQ0EsSUFBTUMsY0FBYyxHQUFHRixRQUFRLENBQUNDLGFBQVQsQ0FBdUIsaUJBQXZCLENBQXZCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdmlydHVhbC1rZXlib2FyZC8uL3NyYy9pbmRleC5qcz9iNjM1Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi9zdHlsZS9tYWluLnNjc3MnO1xyXG5cclxuLypzdGFydCBrZXlib2FyZCovXHJcblxyXG5jb25zdCBrZXlib2FyZCA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoXCIua2V5Ym9hcmRcIik7XHJcbmNvbnN0IGtleWJvYXJkX19rZXlzID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcihcIi5rZXlib2FyZF9fa2V5c1wiKTtcclxuXHJcbiJdLCJuYW1lcyI6WyJrZXlib2FyZCIsImRvY3VtZW50IiwicXVlcnlTZWxlY3RvciIsImtleWJvYXJkX19rZXlzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.js\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style/main.scss */ \"./src/style/main.scss\");\n\n/*start keyboard*/\n\nfunction createHeader() {\n  const container = document.createElement(\"div\");\n  const footer = document.createElement(\"footer\");\n  container.classList.add(\"container\");\n  document.body.appendChild(container);\n  document.body.appendChild(footer);\n  let new_desc_inner = `<h1>Virtual Keyboard w/ Vanilla JS</h1>\n    <h3>Features</h3>\n    <ul>\n        <li>Easy to integrate</li>\n        <li>Responsive</li>\n        <li>Vanilla JS (<strong>no libraries required!</strong>)</li>\n    </ul>\n    <textarea name=\"keyboard_text\" class=\"use-keyboard-input\" rows=\"5\" cols=\"35\"></textarea>`;\n  container.innerHTML = new_desc_inner;\n  let new_footer_inner = `<div class=\"footer__container footer-copyright\">\n    <div class=\"footer-copyright__element\">\n        <p class=\"copyright\">©</p>\n        <p class=\"year\">2022</p>\n        <a class=\"github-username\" href=\"https://github.com/OutLaw0\" target=\"_blank\" rel=\"noopener noreferrer\">github</a>\n    </div>\n    <a href=\"https://rs.school/js/\" class=\"rss\" target=\"_blank\"> Rolling Scopes School </a>\n</div>`;\n  footer.innerHTML = new_footer_inner;\n}\n\nwindow.addEventListener(\"DOMContentLoaded\", function () {\n  createHeader();\n});\nconst Keyboard = {\n  elements: {\n    main: null,\n    keysContainer: null,\n    keys: []\n  },\n  eventHandlers: {\n    oninput: null,\n    onclose: null\n  },\n  properties: {\n    value: \"\",\n    capsLock: false\n  },\n\n  init() {\n    //create elements\n    this.elements.main = document.createElement(\"div\");\n    this.elements.keysContainer = document.createElement(\"div\"); //add class\n\n    this.elements.main.classList.add(\"keyboard\", \"1keyboard--hidden\");\n    this.elements.keysContainer.classList.add(\"keyboard__keys\"); //add to body > container\n\n    this.elements.main.appendChild(this.elements.keysContainer);\n    document.body.firstElementChild.appendChild(this.elements.main);\n  },\n\n  _createKeys() {\n    const fragment = document.createDocumentFragment();\n    const keyLayout = [\"\\`\", \"1\", \"2\", \"3\", \"4\", \"5\", \"6\", \"7\", \"8\", \"9\", \"0\", \"-\", \"=\", \"backspace\", \"Tab\", \"q\", \"w\", \"e\", \"r\", \"t\", \"y\", \"u\", \"i\", \"o\", \"p\", \"[\", \"]\", \"\\\\\", \"caps\", \"a\", \"s\", \"d\", \"f\", \"g\", \"h\", \"j\", \"k\", \"l\", \";\", \"\\'\", \"enter\", \"Shift\", \"z\", \"x\", \"c\", \"v\", \"b\", \"n\", \"m\", \",\", \".\", \"?\", \"Ctrl\", \"Alt\", \"space\"]; //Create HTML icon\n\n    const createIconHtml = icon_name => {\n      return `<i class=\"material-icons\">${icon_name}</i>`;\n    };\n\n    keyLayout.forEach(key => {\n      const keyElement = document.createElement('button');\n      const insertLineBreak = [\"backspace\", \"p\", \"enter\", \"?\"].indexOf(key) !== -1; // Add attributes/classes\n\n      keyElement.setAttribute(\"type\", \"button\");\n      keyElement.classList.add(\"keyboard__key\");\n\n      if (key == \"backspace\") {\n        keyElement.classList.add(\"keyboard__key--wide\");\n        keyElement.innerHTML = createIconHtml(\"backspace\");\n      }\n    });\n  },\n\n  _triggerEvent(handlerName) {\n    console.log(\"Event Trig! Event Name: \" + handlerName);\n  },\n\n  _toggleCapsLock() {\n    console.log(\"Caps Lock Toggled!\");\n  },\n\n  open() {},\n\n  close() {}\n\n};\nwindow.addEventListener(\"DOMContentLoaded\", function () {\n  Keyboard.init();\n});\n\n//# sourceURL=webpack://virtual-keyboard/./src/index.js?");
 
 /***/ }),
 
@@ -24,9 +24,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /*!*****************************!*\
   !*** ./src/style/main.scss ***!
   \*****************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc3R5bGUvbWFpbi5zY3NzLmpzIiwibWFwcGluZ3MiOiI7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3ZpcnR1YWwta2V5Ym9hcmQvLi9zcmMvc3R5bGUvbWFpbi5zY3NzPzFjYzQiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/style/main.scss\n");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://virtual-keyboard/./src/style/main.scss?");
 
 /***/ })
 
@@ -58,21 +58,21 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
+/******/ 	(() => {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
+/******/ 		__webpack_require__.r = (exports) => {
 /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	}();
+/******/ 	})();
 /******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -2,6 +2,273 @@ import './style/main.scss';
 
 /*start keyboard*/
 
-const keyboard = document.querySelector(".keyboard");
-const keyboard__keys = document.querySelector(".keyboard__keys");
+function createHeader() { 
+    const container = document.createElement("div");
+    const footer = document.createElement("footer");
 
+    container.classList.add("container");
+   
+    document.body.appendChild(container)
+    document.body.appendChild(footer)
+
+    const new_desc_inner = `<h1>Virtual Keyboard w/ Vanilla JS</h1>
+    <h3>Features</h3>
+    <ul>
+        <li>Easy to integrate</li>
+        <li>Responsive</li>
+        <li>Vanilla JS (<strong>no libraries required!</strong>)</li>
+    </ul>
+    <textarea name="keyboard_text" class="use-keyboard-input" rows="5" cols="35"></textarea>`;
+    
+    container.insertAdjacentHTML("afterBegin", new_desc_inner);
+
+    const new_footer_inner =
+
+    `<div class="footer__container footer-copyright">
+    <div class="footer-copyright__element">
+        <p class="copyright">©</p>
+        <p class="year">2022</p>
+        <a class="github-username" href="https://github.com/OutLaw0" target="_blank" rel="noopener noreferrer">github</a>
+    </div>
+    <a href="https://rs.school/js/" class="rss" target="_blank"> Rolling Scopes School </a>
+</div>`
+    
+footer.insertAdjacentHTML("afterBegin", new_footer_inner);
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    createHeader();
+})
+
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+    properties: {
+        value: "",
+        capsLock: false,
+        langRU: false,
+    },
+ 
+    keyLayout : [
+        "`","1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=","backspace",
+        "Tab","q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del",
+        "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'","enter",
+        "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+        "Close", "Ctrl", "ChangeLang", "Alt", "space"
+    ],
+
+     keyLayoutRU : [
+        "`","1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=","backspace",
+        "Tab","й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\", "Del",
+        "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э","enter",
+        "Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".",
+        "Close", "Ctrl", "ChangeLang", "Alt", "space"
+    ],
+
+    init() { //create elements
+
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
+        //add class
+        this.elements.main.classList.add("keyboard", "keyboard--hidden1");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+        this.elements.keysContainer.appendChild(this._createKeys());
+        this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+        //add to body > container
+       
+              
+        //prevent losefocus!
+        this.elements.main.addEventListener("mousedown", (e) => {
+           e.preventDefault();
+        }, false);
+
+
+        this.elements.main.appendChild(this.elements.keysContainer)
+        document.body.firstElementChild.appendChild(this.elements.main) 
+  
+        // Automatically use keyboard for elements with .use-keyboard-input
+       document.querySelectorAll(".use-keyboard-input").forEach(element => {
+            element.addEventListener("focus", () => {
+                this.open(element.value, currentValue => {
+                    element.value = currentValue;
+                });
+            });
+        });
+    /*
+        const textArea = document.querySelector(".use-keyboard-input");
+        this.open(textArea.value, currentValue => {
+          textArea.value = currentValue;
+      });*/          
+    },
+  
+    _createKeys() {
+      
+        const fragment = document.createDocumentFragment();
+      
+
+        //Create HTML icon
+        const createIconHtml = (icon_name) => {
+            return `<i class="material-icons">${icon_name}</i>`;
+        };
+        this.keyLayout.forEach(key => {
+            const keyElement = document.createElement('button');
+            const insertLineBreak = ["backspace", "Del", "enter", "?"].indexOf(key) !== -1;
+            // Add attributes/classes
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboard__key");
+
+            if (key == "backspace"){
+
+                keyElement.classList.add("keyboard__key--wide");
+                keyElement.innerHTML = createIconHtml("backspace")
+                keyElement.addEventListener("click", () => {
+                    this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+                    this._triggerEvent("oninput");
+
+                });
+            }
+            else if (key == "caps"){
+                keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
+                keyElement.innerHTML = createIconHtml("keyboard_capslock")
+                keyElement.addEventListener("click", () => {
+                    this._toggleCapsLock();
+                    keyElement.classList.toggle("keyboard__key--active", this.properties.capsLock);
+                });
+            }
+            else if (key == "enter"){
+                keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHtml("keyboard_return");
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value += "\n";
+                        this._triggerEvent("oninput");
+                    });
+                }
+                else if (key == "space"){
+                    keyElement.classList.add("keyboard__key--extra-wide");
+                    keyElement.innerHTML = createIconHtml("space_bar");
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value += " ";
+                        this._triggerEvent("oninput");
+                    });
+                    }
+                else if (key == "ChangeLang"){
+                       keyElement.classList.add("keyboard__key--dark");
+                        keyElement.innerHTML = createIconHtml("language");
+                        keyElement.addEventListener("click", () => {
+                            this._toggleLang();
+                        });
+                        }
+                else if (key == "Close"){
+                    keyElement.classList.add("keyboard__key--wide", "keyboard__key--dark");
+                    keyElement.innerHTML = createIconHtml("check_circle");
+
+                    keyElement.addEventListener("click", () => {
+
+                        this.close();
+                        this._triggerEvent("onclose");
+                    });
+                }
+                 else {
+                        keyElement.textContent = key.toLowerCase();
+    
+                        keyElement.addEventListener("mousedown", () => {
+                         
+                           this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+                           this._triggerEvent("oninput");
+                       
+                        }, false);
+
+                    }
+                        fragment.appendChild(keyElement);
+
+                        if (insertLineBreak) {
+                            fragment.appendChild(document.createElement("br"));
+                        }            
+                  
+                      
+        })
+      
+        return fragment;
+
+    },
+
+    _triggerEvent(handlerName){ 
+     
+        if (typeof this.eventHandlers[handlerName] == "function") {
+            this.eventHandlers[handlerName](this.properties.value);
+       
+        }
+    },
+
+    _toggleCapsLock(){
+        this.properties.capsLock = !this.properties.capsLock;
+        for (let key of this.elements.keys) {
+            if (key.childElementCount === 0) {
+                key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+            }
+        }
+    },
+    _toggleLang(){
+        let array = [];
+       if (this.properties.langRU) {
+           array = this.keyLayout;
+           this.properties.langRU = false
+       }
+       else {
+        array = this.keyLayoutRU;
+        this.properties.langRU = true
+    }
+        for (let i = 0; i < this.elements.keys.length; i++) {
+            if (this.elements.keys[i].childElementCount === 0) {
+                this.elements.keys[i].textContent = array[i];
+            }
+        }
+    },
+
+    open(initialValue, oninput, onclose) {
+        this.properties.value = initialValue || "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.remove("keyboard--hidden");
+        
+    },
+
+    close() {
+        this.properties.value = "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.add("keyboard--hidden");
+    }
+
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init();
+   
+     
+    })
+    
+
+    //TODO
+
+    // 1. Отменить стандартные нажатия клавиатуры, для этого прописать keycode для всех layout
+
+    // 2. Сделать Shift для этого также нужны KeyCode
+
+    // 3. Сделать стрелочки 
+
+    // 4. Сделать ЗАЛИПАНИЕ клавиш
+
+    //5. Сделать Del
+    
+    // PROFIT!!!
+        
